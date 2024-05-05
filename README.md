dcd793882b7290fec7e8960aa01b0ec2eac5b474
cacirom23 <romainlaidet0423@gmail.com>
2024-05-05 05:07:16 -0400

    Projet à jour

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Projet instant weather R209
 
+<<<<<<< HEAD
 ## Collaborator : Laidet Romain, Goubert Louis
+=======
+## Collaborator : Laidet Romain, Goubert Louis, Julien Baptiste
+>>>>>>> 4c1ba35 (Projet à jour)
 
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const axios = require('axios');
 
 // Fonction pour obtenir le code INSEE d'une ville en fonction de son nom
@@ -53,3 +54,90 @@ getCodeInseeParNomVille(nomVille)
                 });
         }
     });
+=======
+document.addEventListener("DOMContentLoaded", () => {
+
+    const postalCodeInput = document.getElementById("code-postal");
+    const villeSelect = document.getElementById("menu-deroulant");
+    const submitButton = document.getElementById("submitbutton");
+
+    async function getCitiesByPostalCode(postalCode) {
+      try {
+        const response = await fetch(`https://geo.api.gouv.fr/communes?codePostal=${postalCode}`);
+        const data = await response.json();
+        console.table(data);
+        return data;
+      } catch (error) {
+        console.error('Erreur:', error);
+      }
+    }
+
+    postalCodeInput.addEventListener('input', async () => {
+      const codePostal = postalCodeInput.value;
+      villeSelect.innerHTML = "";
+      if (codePostal.length === 5) {
+        try {
+          const data = await getCitiesByPostalCode(codePostal);
+          displayCities(data);
+        } catch (error) {
+          console.error(
+            "Une erreur est survenue lors de la recherche de la commune :",
+            error
+          );
+          throw error;
+        }
+      }
+    });
+
+    function displayCities(data) {
+      if (data.length === 1) {
+        const town = data[0];
+        villeSelect.innerHTML = `<option value="${town.code}">${town.nom}</option>`;
+      } else if (data.length > 1) {
+        data.forEach((town) => {
+          const option = document.createElement("option");
+          option.value = town.code;
+          option.textContent = town.nom;
+          villeSelect.appendChild(option);
+        });
+      }
+      villeSelect.style.display = "block";
+      submitButton.style.display = "block";
+    };
+
+    async function getmeteoparville(selectedville) {
+      try {
+        const response = await fetch( `https://api.meteo-concept.com/api/forecast/daily/0?token=1ccbcf76f9a6dc83aa91a60c303d9e7a6180b03278bf1a226dd64e74c60d9f4e&insee=${selectedville}`
+        );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Erreur lors de la requête API:", error);
+      throw error;
+    }
+  }
+
+  function displayHours(sunHours) {
+      return sunHours + (sunHours > 1 ? " heures" : " heure");
+    }
+
+  submitButton.addEventListener("click", async () => {
+  const selectedVille = villeSelect.value;
+  if (selectedVille != null) {
+    try {
+      const data = await getmeteoparville(selectedVille);
+      document.getElementById('temp-min').innerText = `Température minimale : ${data.forecast.tmin}°C`;
+      document.getElementById('temp-max').innerText = `Température maximale : ${data.forecast.tmax}°C`;
+      document.getElementById('prob-pluie').innerText = `Probabilité de pluie : ${data.forecast.probarain}%`;
+      document.getElementById('ensoleillement').innerText = `Ensoleillement journalier : ${displayHours(data.forecast.sun_hours)}`;
+    } catch (error) {
+      console.error("Erreur lors de la requête API meteoConcept:", error);
+      throw error;
+    }
+  }
+});
+
+    
+
+  });
+>>>>>>> 4c1ba35 (Projet à jour)
